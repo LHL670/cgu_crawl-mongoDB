883ddd9f16e272854c7c3bdfe6b9994ce0bc024b
LHL670 <sally12066@gmail.com>
2022-03-14 15:24:37 +0800

    fix

--- pymongo_test.py
+++ pymongo_test.py
@@ -9,10 +9,35 @@ db = cluster["test"]
 collection = db["test"]
 dblst = cluster.list_database_names()
 print(dblst)
+test = {"_id": 'DZ-fHPAAAJ'}
+# post = {"$set": {"name": "test", "updaeTime": "2022-03-14 13:38:00"}}
+e = {'_id': 'DZ-fHPAAAJ',
+     'personalData': {'name': 'Bernhard Schölkopf',
+                      'university': 'None',
+                      'picture': 'https://scholar.googleusercontent.com/citations?view_op=view_photo&user=DZ-fHPgAAAAJ&citpid=2',
+                      'label': ['Machine Learning',
+                                'Causal Inference',
+                                'Artificial Intelligence',
+                                'Computational Photography',
+                                'Statistics'],
+                      'updateTime': '2021-10-09 13:52:05'},
+     'cited': {'citations': {'All': '177951', 'Since2016': '80835'},
+               'h_index': {'All': '163', 'Since2016': '115'},
+               'i10_index': {'All': '577', 'Since2016': '484'}},
+     'citedRecord': [{'updateTime': '2021-10-09 13:52:05', 'cited': {'citations': {'All': '177951', 'Since2016': '80835'},
+                                                                     'h_index': {'All': '163', 'Since2016': '115'},
+                                                                     'i10_index': {'All': '577', 'Since2016': '484'}}}, {'updateTime': '2021-03-14 13:52:05', 'cited': {'citations': {'All': '177951', 'Since2016': '80835'},
+                                                                                                                                                                        'h_index': {'All': '163', 'Since2016': '115'},
+                                                                                                                                                                        'i10_index': {'All': '577', 'Since2016': '484'}}}]}
+a = {"$push": {'citedRecord': {"$each": [{'updateTime': '"2022-03-14 15:38:00', 'cited': {'citations': {'All': '177951', 'Since2016': '80835'},
+                                                                                          'h_index': {'All': '163', 'Since2016': '115'},
+                                                                                          'i10_index': {'All': '577', 'Since2016': '484'}}}]}}}
+# {$push:{"citedRecord":{$each:[{'updateTime': '"2022-03-14 15:38:00', 'cited': {'citations': {'All': '177951', 'Since2016': '80835'},
+#                                                                                    'h_index': {'All': '163', 'Since2016': '115'},
+#                                                                                    'i10_index': {'All': '577', 'Since2016': '484'}}}]}}}
+collection.update_one(test, a)
 
-post = {"_id": 3, "name": "test", "updaeTime": "2022-03-14 13:11:00"}
-
-collection.insert_one(post)
+# collection.insert_one(e)
 
 # myclient = pymongo.MongoClient(
 #     "mongodb+srv://CGUScholar:cguscholarpwd@cluster0.bpq9j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
