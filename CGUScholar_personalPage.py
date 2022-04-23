f8f807dff38d6f25d684664048eba34de03bd9c9
LHL670 <sally12066@gmail.com>
2022-04-23 22:40:42 +0800

    fix

--- CGUScholar_personalPage.py
+++ CGUScholar_personalPage.py
@@ -84,7 +84,7 @@ if __name__ == '__main__':
 
     # update oldest label then crwal user profile
     while(1):
-        label = 'High_Energy_Density_Physics'
+        # label = 'High_Energy_Density_Physics'
         label = manageMongodb.get_labelforCGUScholar()
         CGUScholar_LabelDomain.LabelCrawl(label)
         CGUCrawlWorker(label)
